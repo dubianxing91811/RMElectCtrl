a2d01e7b07244e7560ef0d6924afc3f3e2adfe63
dubianxing91811 <dubianxing91811@gmail.com>
2025-10-12 00:50:47 +0800

    Add files via upload

--- 第 1 周/代码/2/T2.c
+++ 第 1 周/代码/2/T2.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+int x=0;
+scanf("%d",&x);
+while(x!=-1)
+{
+    if (x==0)
+    {
+        printf("helloworld");
+    }else if (x==1)
+    {
+        printf("HELLOWORLD");
+    }
+    scanf("%d",&x);
+}    
+    return 0;
+}
