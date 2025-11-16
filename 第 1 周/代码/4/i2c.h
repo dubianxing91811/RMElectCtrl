7d4bd5bd904e523d7fee7978beb82d3534e87b46
dubianxing91811 <dubianxing91811@gmail.com>
2025-11-16 21:23:16 +0800

    Update i2c.h

--- 第 1 周/代码/4/i2c.h
+++ 第 1 周/代码/4/i2c.h
@@ -1,1 +1,12 @@
+#ifndef __I2C_H
+#define __I2C_H
 
+#include "RM.h"   
+
+void I2C_Start(void);
+void I2C_Stop(void);
+void I2C_SendByte(unsigned char data);
+uint8_t I2C_ReceiveAck(void);
+void I2C_SendData(unsigned char data);
+
+#endif
